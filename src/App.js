0e554a2daed6c5485f12fbd3917882711699e450
joseph-p-pasaoa <joseph.p.pasaoa@gmail.com>
2019-12-02 16:12:39 -0500

    0.3, credits display & increment button added

--- src/App.js
+++ src/App.js
@@ -7,20 +7,36 @@ React Clicker Game Lab
 /* IMPORTS */
 import React from 'react';
 import './App.css';
-const { log } = require('./utils/helpers.js');
+// const { log } = require('./utils/helpers.js');
 
 
 /* EXEC */
 class App extends React.Component {
   constructor() {
     super();
-    this.state = {};
+    this.state = {
+      credits: 0,
+      perClick: 1
+    };
+  }
+
+  handleClickGo = () => {
+    this.setState({
+        credits: this.state.credits + this.state.perClick
+    });
   }
 
   render() {
+    const { credits, perClick } = this.state;
     return (
       <div className="App">
-        {log("Hello")};
+        <div id="grid-base">
+          <div id="topDisplay">
+            <p id="score">{credits}</p>
+            <p>credits</p>
+          </div>
+          <button id="btnGo" onClick={this.handleClickGo}>{`+${perClick.toString()} credits`}</button>
+        </div>
       </div>
     );
   }

3c1d4b52672b592b18d92f5d217ac17867d982c1
joseph-p-pasaoa <joseph.p.pasaoa@gmail.com>
2020-01-22 19:54:25 -0500

    Updates

---
+++ precache-manifest.201433f309497ff84156bdbf0fa02719.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26ea1faab22673479ec704247bf53812",
+    "url": "/lab02_react-states/index.html"
+  },
+  {
+    "revision": "d9949ebd4ae050cef86f",
+    "url": "/lab02_react-states/static/css/main.f33b74ac.chunk.css"
+  },
+  {
+    "revision": "0654392123409900b714",
+    "url": "/lab02_react-states/static/js/2.812a88e5.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/lab02_react-states/static/js/2.812a88e5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d9949ebd4ae050cef86f",
+    "url": "/lab02_react-states/static/js/main.d612491f.chunk.js"
+  },
+  {
+    "revision": "4e19bac5bebe4acd787b",
+    "url": "/lab02_react-states/static/js/runtime-main.ecb23c27.js"
+  },
+  {
+    "revision": "fc49f75587c4793d4900d1f180838502",
+    "url": "/lab02_react-states/static/media/Calistoga-Regular.fc49f755.ttf"
+  },
+  {
+    "revision": "9122299e476671f970e1670bd7b900c8",
+    "url": "/lab02_react-states/static/media/FrederickatheGreat-Regular.9122299e.ttf"
+  }
+]);
